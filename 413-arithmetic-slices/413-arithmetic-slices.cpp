e9a57e23a0b37bede1949e02869f052d7060be39
Nada Adel <nadel6567@yahoo.com>
2022-01-24 18:29:03 +0200

    Time: 8 ms (9.07%), Space: 7.3 MB (52.18%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -1,22 +1,8 @@
 class Solution {
 public:
-    int nCr(int n, int r)
-    {
-        return fact(n) / (fact(r) * fact(n - r));
-    }
- 
-    // Returns factorial of n
-    long fact(int n)
-    {
-        long res = 1;
-        for (int i = 2; i <= n; i++)
-            res = res * i;
-        return res;
-    }
- 
     int numberOfArithmeticSlices(vector<int>& nums) {
         int res=0;
-        int count=1;
+        int count=0;
         vector<int>num;
         for(int i=1; i<nums.size(); i++){
             num.push_back(nums[i]-nums[i-1]);
@@ -26,13 +12,13 @@ public:
                 count++;
             }
             else{
-                res+=nCr(count,2);
-                count=1;
+                res+=(count+1)*count/2;
+                count=0;
             }
         }
         if(count>0){
             
-            res+=nCr(count,2);
+            res+=(count+1)*count/2;
         }
         return res;
     }
