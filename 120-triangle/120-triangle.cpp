d187dbfd8a5d674ff77ece9a027f881c9bce7704
Nada Adel <nadel6567@yahoo.com>
2022-02-13 15:42:25 +0200

    Time: 11 ms (34.01%), Space: 8.6 MB (88.98%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int num_layers = triangle.size(); 
-        vector<int>minlen(triangle.back());
+        vector<int>minsum(triangle.back());
         for(int layer = num_layers - 2; layer >= 0; layer--) {
             for(int node = 0; node <= layer; node++) {
-                minlen[node] = min(minlen[node], minlen[node + 1]) + triangle[layer][node];
+                minsum[node] = min(minsum[node], minsum[node + 1]) + triangle[layer][node];
              } 
         }
-        return minlen[0];
+        return minsum[0];
     }
     
 
