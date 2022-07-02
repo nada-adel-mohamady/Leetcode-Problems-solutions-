42fdca585f445ca33218e4cf43ee8df4e31a5e3c
Nada Adel <nadel6567@yahoo.com>
2022-07-02 09:23:02 +0200

    Time: 132 ms (20.02%), Space: 32.1 MB (95.37%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
@@ -3,8 +3,8 @@ public:
     int maxArea(int h, int w, vector<int>& horizontalCuts, vector<int>& verticalCuts) {
         sort(horizontalCuts.begin(), horizontalCuts.end());
         sort(verticalCuts.begin(), verticalCuts.end());
-        int maxH = horizontalCuts[0];
-        int maxV = verticalCuts[0];
+        int maxH = max(horizontalCuts[0], h - horizontalCuts.back()) ;
+        int maxV = max(verticalCuts[0], w - verticalCuts.back());
         
         for(int i = 1; i < horizontalCuts.size(); i++) {
             maxH = max(maxH, horizontalCuts[i] - horizontalCuts[i-1]);
@@ -12,8 +12,7 @@ public:
         for(int i = 1; i < verticalCuts.size(); i++) {
             maxV = max(maxV, verticalCuts[i] - verticalCuts[i-1]);
         }
-        maxH = max(maxH, h - horizontalCuts.back());
-        maxV = max(maxV, w - verticalCuts.back());
+        
         long res = (long)maxH * (long)maxV % (1000000000 + 7);
         return res;
     }
