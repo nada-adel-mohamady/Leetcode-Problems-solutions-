f80eafb45b39df2768764dcef805abf0904a9246
Nada Adel <nadel6567@yahoo.com>
2022-07-18 23:27:40 +0200

    Time: 4 ms (41.48%), Space: 6.4 MB (78.42%) - LeetHub

--- 2315-count-asterisks/2315-count-asterisks.cpp
+++ 2315-count-asterisks/2315-count-asterisks.cpp
@@ -4,12 +4,8 @@ public:
         int count = 0;
         bool insideBar = false;
         for (int i = 0; i < s.size(); i++) {
-            if (s[i] == '|') {
-                insideBar = !insideBar;
-            }
-            else if (s[i] == '*' && !insideBar) {
-                count++;
-            }
+            if (s[i] == '|') insideBar = !insideBar;
+            else if (s[i] == '*' && !insideBar) count++;
         }
         return count;
     }
