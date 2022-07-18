e9a67e96a09899bda37504c330bf4f9cc2c87f87
Nada Adel <nadel6567@yahoo.com>
2022-07-18 23:20:59 +0200

    Time: 2 ms (66.33%), Space: 6.5 MB (37.81%) - LeetHub

--- 2315-count-asterisks/2315-count-asterisks.cpp
+++ 2315-count-asterisks/2315-count-asterisks.cpp
@@ -2,15 +2,12 @@ class Solution {
 public:
     int countAsterisks(string s) {
         int count = 0;
-        bool insideBar = true;
+        bool insideBar = false;
         for (int i = 0; i < s.size(); i++) {
-            if (s[i] == '|' && !insideBar) {
-                insideBar = true;
+            if (s[i] == '|') {
+                insideBar = !insideBar;
             }
-            else if (s[i] == '|') {
-                insideBar = false;
-            }
-            else if (s[i] == '*' && insideBar) {
+            else if (s[i] == '*' && !insideBar) {
                 count++;
             }
         }
