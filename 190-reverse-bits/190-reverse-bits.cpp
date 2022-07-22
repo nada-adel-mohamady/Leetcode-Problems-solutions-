4ee2c831ac495bfe135f7eb66304086f9003feeb
Nada Adel <nadel6567@yahoo.com>
2022-07-22 07:06:46 +0200

    Time: 9 ms (7.35%), Space: 6 MB (51.78%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.cpp
+++ 190-reverse-bits/190-reverse-bits.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
         uint32_t num = 0;
-        uint32_t zeros = 0;
         for (int i = 0; i < 32; i++) {
             num = num * 2;
             if(n % 2 == 1) {
