4bdf593f2bf515e18df3d7f3a810182732fa4329
Nada Adel <nadel6567@yahoo.com>
2022-07-25 23:28:48 +0200

    Time: 5 ms (78.23%), Space: 7.3 MB (90.39%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -3,20 +3,24 @@ public:
     bool isPalindrome(string s) {
         int leftPointer = 0;
         int rightPointer = s.size() - 1;
+        bool isalphanumeric1, isalphanumeric2;
         while (leftPointer < rightPointer) {
-            if (!iswalnum(s[leftPointer])) {
-                leftPointer++;
-            }
-            if (!iswalnum(s[rightPointer])) {
-                rightPointer--;
-            }
-            if (iswalnum(s[leftPointer]) && iswalnum(s[rightPointer]) && tolower(s[leftPointer]) == tolower(s[rightPointer])) {
-                leftPointer++;
-                rightPointer--;
-            }
-            if (iswalnum(s[leftPointer]) && iswalnum(s[rightPointer]) && tolower(s[leftPointer]) != tolower(s[rightPointer])) {
+            isalphanumeric1 = iswalnum(s[leftPointer]);
+            isalphanumeric2 = iswalnum(s[rightPointer]);  
+             if (isalphanumeric1 && isalphanumeric2 && tolower(s[leftPointer]) != tolower(s[rightPointer])) {
                 return false;
             }
+            if (!isalphanumeric1) {
+                leftPointer++;
+            }
+            if (!isalphanumeric2) {
+                rightPointer--;
+            }
+            if(isalphanumeric1 && isalphanumeric2) {
+                leftPointer++;
+                rightPointer--;
+            }
+           
         }
         return true;
     }
