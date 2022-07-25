39d5204ea9a453e3ea75b0e4c5de9bc5b9d0fb62
Nada Adel <nadel6567@yahoo.com>
2022-07-25 07:48:32 +0200

    Time: 11 ms (56.07%), Space: 7.5 MB (96.26%) - LeetHub

--- 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.cpp
@@ -4,26 +4,27 @@ public:
         int i = 0;
         int j = s.size() - 1;
         int swap;
-        char char1, char2;
-        while (i < j) {
-            char1 = tolower(s[i]);       
-            if(char1 == 'a' || char1 == 'e' || char1 == 'i' || char1 == 'o' || char1 == 'u') {
-                while (j > i) {
-                    //cout<<"char "<<char1<<" char2 "<<char2<<endl;
-                    char2 = tolower(s[j]);
-                    if(char2 == 'a' || char2 == 'e' || char2 == 'i' || char2 == 'o' || char2 == 'u') {
-                        swap = s[i];
-                        s[i] = s[j];
-                        s[j] = swap;
-                        j--;
-                        break;
-                    }
-                    j--;     
-                }
-                
+        int char1, char2;
+        while (i < j) {   
+            char1 = tolower(s[i]);
+            while (i < j && !(char1 == 'a' || char1 == 'e' || char1 == 'i' || char1 == 'o' || char1 == 'u')) {
+                i++;
+                char1 = tolower(s[i]);
             }
-            i++;
+            char2 = tolower(s[j]);
+            while (i < j && !(char2 == 'a' || char2 == 'e' || char2 == 'i' || char2 == 'o' || char2 == 'u')) {
+                j--;
+                char2 = tolower(s[j]);
+            }
+            if(i < j) {
+                swap = s[i];
+                s[i] = s[j];
+                s[j] = swap;
+                i++;
+                j--;
+            }       
         }
         return s;
     }
+
 };
