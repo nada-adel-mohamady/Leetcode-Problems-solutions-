4b68b6b083daf388b05bc66e23fb0ee1d18a214a
Nada Adel <nadel6567@yahoo.com>
2022-07-25 07:27:47 +0200

    Time: 14 ms (34.03%), Space: 7.6 MB (96.26%) - LeetHub

--- 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.cpp
@@ -4,26 +4,27 @@ public:
         int i = 0;
         int j = s.size() - 1;
         int swap;
-        char char1, char2;
-        while (i < j) {
-            char1 = tolower(s[i]);       
-            if(char1 == 'a' || char1 == 'e' || char1 == 'i' || char1 == 'o' || char1 == 'u') {
-                while (j > i) {
-                    //cout<<"char "<<char1<<" char2 "<<char2<<endl;
-                    char2 = tolower(s[j]);
-                    if(char2 == 'a' || char2 == 'e' || char2 == 'i' || char2 == 'o' || char2 == 'u') {
-                        swap = s[i];
-                        s[i] = s[j];
-                        s[j] = swap;
-                        j--;
-                        break;
-                    }
-                    j--;     
-                }
-                
+        while (i < j) {   
+            while (i < j && !isVowel(s[i])) {
+                i++;
             }
-            i++;
+            while (i < j && !isVowel(s[j])) {
+                j--;
+            }
+            if(i < j) {
+                swap = s[i];
+                s[i] = s[j];
+                s[j] = swap;
+                i++;
+                j--;
+            }       
         }
         return s;
+    }
+    
+    bool isVowel(char c) {
+        c = tolower(c);
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') return true;
+        return false;
     }
 };
