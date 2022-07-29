8d9583d399e7fbcef548f168e9d77611260935fc
Nada Adel <nadel6567@yahoo.com>
2022-07-29 17:23:48 +0200

    Time: 29 ms (16.00%), Space: 8.3 MB (84.66%) - LeetHub

---
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char, int>mp;
+        priority_queue<pair<int, char> > pq;
+        string str = "";
+        char c;
+        pair<int, char>tmp;
+        for (auto c: s) {
+            mp[c]++;
+        }
+        for (auto c: s) {
+            if (mp[c])
+                pq.push(make_pair(mp[c], c));
+            mp[c] = 0;
+        }
+        while (!pq.empty()) {
+            tmp = pq.top();
+            pq.pop();
+            c = tmp.second;
+            for (int i = 0; i < tmp.first; i++) {
+                str+=c;
+            }
+            mp[c] = 0;
+        }
+        return str;
+    }
+};
